7ca8ec661308cc2e20afc8bb549c6863c008a8a1
Julio Cesar Navarro <jcnavarro066@misena.edu.co>
2020-10-14 13:31:37 -0500

    Basi Login form is created

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app= Flask(__name__)
 
 @app.route('/')
-
 def hello():
     return 'Hello'
+
+@app.route('/login')
+def login():
+    return render_template("index.html")
