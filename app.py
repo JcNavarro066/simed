95d7354c0a5cd0253a092ecdba16f0b38a11d374
Julio Cesar Navarro <jcnavarro066@misena.edu.co>
2020-10-15 00:22:01 -0500

    Modified /index, create /user

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_db2 import DB2
 
 app = Flask(__name__)
@@ -13,14 +13,23 @@ app.config['DB2_PASSWORD'] = '14ww1r21s31q8l@b'
 db = DB2(app)
 
 
-@app.route('/home')
+@app.route('/')
 def hello():
     return 'Hello'
 
 
-@app.route('/')
+@app.route('/user', methods=['POST', 'GET'])
 def login():
-    return render_template('index.html')
+    if request.method == 'POST':
+        F_Name = request.form['F_Name']
+        L_Name = request.form['L_Name']
+        #cur = db.connection.cursor()
+        #cur.execute('INSERT INTO tblUser (F_Name \, L_Name) VALUES (%s, %s, %s)', (F_Name, L_Name))
+        #response = cur.fetchall()
+        print(F_Name)
+        print(L_Name)
+
+    return render_template('user.html')
 
 
 @app.route('/users')
