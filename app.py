29c3912556a6b025661188ae61bc202c77a2cdc1
Julio Cesar Navarro <jcnavarro066@misena.edu.co>
2020-10-14 20:53:45 -0500

    Database conection

--- app.py
+++ app.py
@@ -1,21 +1,31 @@
 from flask import Flask, render_template
-#from flask_db2 import DB2
+from flask_db2 import DB2
 
-app= Flask(__name__)
+app = Flask(__name__)
 
-#app.config['DB2_DATABASE'] =
-#app.config['DB2_HOSTNAME'] =
-#app.config['DB2_PORT'] =
-#app.config['DB2_USER'] =
-#app.config['DB2_PASSWORD'] =
+app.config['DB2_DATABASE'] = 'BLUDB'
+app.config['DB2_HOSTNAME'] = 'dashdb-txn-sbox-yp-dal09-08.services.dal.bluemix.net'
+app.config['DB2_PORT'] = 50000
+app.config['DB2_PROTOCOL'] = 'TCPIP'
+app.config['DB2_USER'] = 'vnx75949'
+app.config['DB2_PASSWORD'] = '14ww1r21s31q8l@b'
 
-#db = DB2
+db = DB2(app)
+
 
 @app.route('/home')
 def hello():
     return 'Hello'
 
+
 @app.route('/')
 def login():
     return render_template('index.html')
 
+
+@app.route('/users')
+def users():
+    cur = db.connection.cursor()
+    cur.execute('SELECT * FROM tblUser')
+    response = cur.fetchall()
+    print(response)
