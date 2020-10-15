48d77478c0605bf0fa387ca47ec31bf955d48ca9
Julio Cesar Navarro <jcnavarro066@misena.edu.co>
2020-10-14 19:01:05 -0500

    edit home forms

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from flask import Flask, render_template
+#from flask_db2 import DB2
 
 app= Flask(__name__)
 
-@app.route('/')
+#app.config['DB2_DATABASE'] =
+#app.config['DB2_HOSTNAME'] =
+#app.config['DB2_PORT'] =
+#app.config['DB2_USER'] =
+#app.config['DB2_PASSWORD'] =
+
+#db = DB2
+
+@app.route('/home')
 def hello():
     return 'Hello'
 
-@app.route('/login')
+@app.route('/')
 def login():
-    return render_template("index.html")
+    return render_template('index.html')
+
